7c4bce3df7b1e4b482c005c16ab19fafe347a414
hkwaller <hkwaller@gmail.com>
2014-08-22 11:47:17 +0200

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,86 @@
+angular.module('starter.controllers', [])
+
+.controller('LoanCtrl', function($scope, $location, FirebaseService) {
+    $scope.loans = FirebaseService.loans();
+    
+    $scope.return = function(e) {
+        $location.path("/tab/return/" + e.$id);
+      }
+    $scope.data = {'showDelete':true};
+})
+
+.controller('EquipCtrl', function($scope, $location, FirebaseService) {
+  $scope.equipment = FirebaseService.equipment();
+    
+  $scope.startLoan = function(e) {
+    $location.path("/tab/loan/" + e.$id);
+  }
+  
+  $scope.gotoAdd = function() {
+    $location.path("/tab/add/");
+  }
+  
+})
+
+.controller('DetailCtrl', function($scope, $stateParams, $location, FirebaseService) {
+    $scope.equipment = FirebaseService.equipment();
+    $scope.e = $scope.equipment.$getRecord($stateParams.id);
+    
+    $scope.availability = function(product) {
+        if (product.available) return "Ledig";
+        else return "Utlånt";
+    };
+    
+    $scope.startLoan = function() {
+        console.log("test");
+        $location.path("/tab/loan/" + $scope.e.$id);
+      }
+})
+
+.controller('StartLoanCtrl', function($scope, $stateParams, $location, FirebaseService) {
+    $scope.loans = FirebaseService.loans();
+    $scope.equipment = FirebaseService.equipment();
+    
+    $scope.e = $scope.equipment.$getRecord($stateParams.id);
+    
+    $scope.loan = {'device':$scope.e.make + " " + $scope.e.model, 'days':7};
+
+    $scope.startLoan = function(loan) {
+        $scope.e.available = false;
+        loan.date = new Date();
+        loan.id = $scope.e.$id;
+        $scope.loans.$add(loan);
+        $scope.equipment.$save($scope.equipment.indexOf($scope.e));
+        $location.path('/utstyr/');
+    };
+
+})
+
+.controller('AddCtrl', function($scope, $location, FirebaseService) {
+      $scope.equipment = FirebaseService.equipment();
+
+    $scope.return = function(e) {
+        $location.path("/tab/return/" + e.$id);
+      }
+    
+    $scope.add = function(product) {
+        product.available = true;
+        $scope.equipment.$add(product);
+        $location.path("/utstyr/");
+    }
+})
+.controller('ReturnCtrl', function($scope, $stateParams, $location, FirebaseService) {
+    $scope.equipment = FirebaseService.equipment();
+    $scope.loans = FirebaseService.loans();
+
+    $scope.loan = $scope.loans.$getRecord($stateParams.id);
+    $scope.e = $scope.equipment.$getRecord($scope.loan.id);
+    
+    $scope.return = function() {
+        $scope.e.available = true;
+        $scope.loans.$remove($scope.loan);
+        $scope.equipment.$save($scope.equipment.indexOf($scope.e));
+        $location.path("/tab/loans/");
+    }
+    
+});
